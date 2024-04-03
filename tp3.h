8c31ac9e0f342feb095b4364e9f5625dc83d9cb5
Remi8404 <72575793+Remi8404@users.noreply.github.com>
2024-04-03 11:46:46 +0200

    Update tp3.h

--- tp3.h
+++ tp3.h
@@ -1,1 +1,15 @@
+typedef struct Sommet{
+  int id;
+  struct Sommet *s; //sommet suivant (ordre croissant)
+  struct Voisin *v;
+};
+
+typedef struct Voisin{
+  int id;
+  struct Voisin *v;
+};
+
+typedef struct Graphe{
+  struct Sommet *s;//1er sommet du graphe
+};
 
