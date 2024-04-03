334d8154b43f82d32d003a6948f791434ac3eba9
Remi8404 <72575793+Remi8404@users.noreply.github.com>
2024-04-03 11:52:00 +0200

    Update tp3.h

--- tp3.h
+++ tp3.h
@@ -1,3 +1,4 @@
+//déclaration des structures
 typedef struct Sommet{
   int id;
   struct Sommet *s; //sommet suivant (ordre croissant)
@@ -13,3 +14,26 @@ typedef struct Graphe{
   struct Sommet *s;//1er sommet du graphe
 };
 
+//Prototypes des fonctions
+//Fonctions demandées
+graphe* creerGraphe(); 
+
+void creerSommet(graphe *g, int id);
+
+sommet* rechercherSommet(graphe g, int id);
+
+void ajouterArete(graphe *g, int id1, int id2);
+
+graphe* construireGraphe(int N);
+
+void afficherGraphe(graphe g);
+
+int rechercherDegre(graphe g);
+
+void supprimerSommet(graphe *g, int id);
+
+int contientBoucle(graphe g);
+
+void fusionnerSommet(graphe *g, int idSommet1, int idSommet2);
+
+//Fonctions rajoutées
